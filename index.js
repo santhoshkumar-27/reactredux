2b1d5d473c032c639ec365cafb19e9fcabe56ba5
santhoshkumar <its.santhoshkumar@outlook.com>
2024-02-18 19:24:36 +0530

    fix: with custom element

--- index.js
+++ index.js
@@ -22,12 +22,20 @@ let stringTrim = sts => sts.trim();
 let warpInDiv = strs => `<div>${strs}</div>`;
 
 let warpInSpan = strs => `<span>${strs}</span>`;
+/**
+ * @description create a element with a input of element name and string
+ */
+const warpInElement = (element, content) =>  `<${element}>${content}</${element}>`
 // function compositions
 console.log(warpInDiv(stringTrim(' asdfasfdasfdasdf ')))
-// console.log(warpInSpan(stringTrim(' asdfasfdasfdasdf ')))
+console.log(warpInSpan(stringTrim(' asdfasfdasfdasdf ')))
+console.log(warpInElement('span', stringTrim(' asdfasfdasfdasdf ')))
 
 // for avoiding multiple compose functions
 
-const transform = pipe(stringTrim, warpInDiv)
+// const transform = pipe(stringTrim, warpInDiv)
+
+// not able to send element in this approach
+// const transform = pipe(stringTrim, warpInElement)
 
 console.log(' sdfsadfdsfsdfsd  ')
