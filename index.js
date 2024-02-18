d51748eb5d12f3f67d383a6f4ab910573cd0572c
santhoshkumar <its.santhoshkumar@outlook.com>
2024-02-18 12:25:49 +0530

    fix: functional based approch

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function pressLike() {
-    return 'this is called'
+    return 'this is called';
 }
 
 function handleFun(fn) {
-    console.log(fn())
+    console.log(fn());
 }
 
 
-handleFun(pressLike)
+handleFun(pressLike);
+
+// handle task
+let input = " subscribe ";
+let output = `<div>${input.trim()}</div>`
+
+// Approch functional based
+
+let stringTrim = sts => sts.trim();
+
+let warpInDiv = strs => `<div>${strs}</div>`
+
+
+console.log(warpInDiv(stringTrim(' asdfasfdasfdasdf ')))
