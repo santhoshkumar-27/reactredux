aaafb658b35cd50703d7225f51aa358bd13fedb3
santhoshkumar <its.santhoshkumar@outlook.com>
2024-02-24 12:27:50 +0530

    doing nested object have same reference

--- nsobj.js
+++ nsobj.js
@@ -23,5 +23,9 @@ const firstCopy = {
 firstCopy.name = '111111111'
 // but only doing parent object modified it doesn't affect the original parent object
 
+// now the  nest object has the same reference to the original
+firstCopy.address.area = 'chinna veerampattinam'
+// in order to make avoid, we need to deep copy the nested object
+
 console.log('firstCopy', firstCopy)
 console.log('after', original)
